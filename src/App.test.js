0dab84df1b0d3f30c69eccc5260da67acb704859
Afrin Zahan Sreety <zahansreety@gmail.com>
2024-10-09 04:56:23 +0600

    added margin

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Result', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Result/i);
   expect(linkElement).toBeInTheDocument();
 });
